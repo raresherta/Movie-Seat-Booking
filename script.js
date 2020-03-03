5df271a0b0a15061997fe927cd31291e4694a062
Rares Herta <rares.herta@endava.com>
2020-03-03 10:25:49 +0200

    Save data to Local Storage

--- script.js
+++ script.js
@@ -9,15 +9,33 @@ let ticketPrice = parseInt(movieSelect.value);
 // Update total and count
 function updateSelectedCount() {
 	const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+	// Selected seats returns a nodelist and we can't save the nodelist to the local storage
+	// Copy selected seats into an array
+	// Map through array
+	// Return a new array of indexes
+	const seatsIndex = [...selectedSeats].map(function(seat) {
+		return [...seats].indexOf(seat);
+	});
+
+	localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
 	const selectedSeatsCount = selectedSeats.length;
 
 	count.innerText = selectedSeatsCount;
 	total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+	localStorage.setItem('selectedMovieIndex', movieIndex);
+	localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // Movie select event
 movieSelect.addEventListener('change', e => {
 	ticketPrice = parseInt(e.target.value);
+	setMovieData(e.target.selectedIndex, e.target.value);
 	updateSelectedCount();
 });
 
